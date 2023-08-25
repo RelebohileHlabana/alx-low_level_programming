26a1517ac69d8cc170e861248db075c4bc13546d
RelebohileHlabana <relebohilehlabana96@gmail.com>
2023-08-25 07:18:36 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,31 +8,26 @@
  */
 int _strlen(char *s)
 {
-	int length = 0;
-
-	while (*s)
-	{
-		length++;
-		s++;
-	}
-	return (length);
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen(s + 1));
 }
 
 /**
- * is_palindrome_helper - Check if a string is a palindrome (helper function).
- * @s: The string to check.
+ * _check_palindrome - Check if a string is a palindrome.
+ * @s: The string.
  * @start: The starting index.
  * @end: The ending index.
  *
  * Return: 1 if the string is a palindrome, otherwise 0.
  */
-int is_palindrome_helper(char *s, int start, int end)
+int _check_palindrome(char *s, int start, int end)
 {
 	if (start >= end)
 		return (1);
 	if (s[start] != s[end])
 		return (0);
-	return (is_palindrome_helper(s, start + 1, end - 1));
+	return (_check_palindrome(s, start + 1, end - 1));
 }
 
 /**
@@ -47,5 +42,5 @@ int is_palindrome(char *s)
 
 	if (length <= 1)
 		return (1);
-	return (is_palindrome_helper(s, 0, length - 1));
+	return (_check_palindrome(s, 0, length - 1));
 }
